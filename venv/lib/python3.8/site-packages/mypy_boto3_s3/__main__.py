a30239c52af1bc991d3fcc5c3916c0b6d25b327a
sujata <sujata16k@gmail.com>
2022-11-06 14:10:25 +0530

    commit

---
+++ venv/lib/python3.8/site-packages/mypy_boto3_s3/__main__.py
@@ -0,0 +1,38 @@
+"""
+Main CLI entrypoint.
+"""
+import sys
+
+
+def print_info() -> None:
+    """
+    Print package info to stdout.
+    """
+    print(
+        "Type annotations for boto3.S3 1.24.76\nVersion:         1.24.76\nBuilder version:"
+        " 7.11.9\nDocs:           "
+        " https://youtype.github.io/boto3_stubs_docs/mypy_boto3_s3//\nBoto3 docs:     "
+        " https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3\nOther"
+        " services:  https://pypi.org/project/boto3-stubs/\nChangelog:      "
+        " https://github.com/youtype/mypy_boto3_builder/releases"
+    )
+
+
+def print_version() -> None:
+    """
+    Print package version to stdout.
+    """
+    print("1.24.76")
+
+
+def main() -> None:
+    """
+    Main CLI entrypoint.
+    """
+    if "--version" in sys.argv:
+        return print_version()
+    print_info()
+
+
+if __name__ == "__main__":
+    main()
